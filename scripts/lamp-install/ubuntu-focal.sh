19180c5ce0d882de5f93c418f5fc5b8dd0fe3017
petergerner <pgerner@gmail.com>
2020-06-17 11:44:24 +0200

    hostname as argument

--- scripts/lamp-install/ubuntu-focal.sh
+++ scripts/lamp-install/ubuntu-focal.sh
@@ -60,17 +60,22 @@ echo "ÆGIR | ------------------------------------------------------------------
 unset fqdn
 unset result
 echo "ÆGIR | Default hostname is $AEGIR_HOST ..."
-read -p "Enter your FQDN hostname here (or press enter to continue with default): " fqdn
-# check valid FQDN syntax: https://stackoverflow.com/questions/32909454/evaluation-of-a-valid-fqdn-on-bash-regex
-result=`echo $fqdn | grep -P '(?=^.{1,254}$)(^(?>(?!\d+\.)[a-zA-Z0-9_\-]{1,63}\.?)+(?:[a-zA-Z]{2,})$)'`
-if [[ -z "$result" ]]
+# take new hostname from first argument
+if [[ -z "$1" ]]
 then
-    echo "User error: $fqdn is NOT a FQDN. Exiting ..."
+    echo "No user input, using default hostname: $AEGIR_HOST"
 else
-    # $fqdn is a FQDN
-    AEGIR_HOST=$fqdn
+   # check valid FQDN syntax: https://stackoverflow.com/questions/32909454/evaluation-of-a-valid-fqdn-on-bash-regex
+   result=`echo $1 | grep -P '(?=^.{1,254}$)(^(?>(?!\d+\.)[a-zA-Z0-9_\-]{1,63}\.?)+(?:[a-zA-Z]{2,})$)'`
+   if [[ -z "$result" ]]
+   then
+    echo "Error in user input: $1 is NOT a FQDN. Exiting ..."
+    exit 1
+   else
+       # $fqdn is a FQDN
+       AEGIR_HOST=$1
+   fi
 fi
-echo "ÆGIR | Continuing with hostname: $AEGIR_HOST"
 sudo hostnamectl set-hostname "$AEGIR_HOST"
 
 # Install required OS packages for Aegir
